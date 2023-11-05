d34f7c511c15e5a349b1ed755adff28d07d4f22f
jickDo <pocard0117@naver.com>
2023-11-05 16:39:08 +0900

    docker setting && docker-compose setting

---
+++ backend/src/main/kotlin/cloudNative/limjickchan/LimjickchanApplication.kt
@@ -0,0 +1,11 @@
+package cloudNative.limjickchan
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LimjickchanApplication
+
+fun main(args: Array<String>) {
+    runApplication<LimjickchanApplication>(*args)
+}
---
+++ backend/src/test/kotlin/cloudNative/limjickchan/LimjickchanApplicationTests.kt
@@ -0,0 +1,13 @@
+package cloudNative.limjickchan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class LimjickchanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
